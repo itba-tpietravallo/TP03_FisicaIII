f0b8d09873fdd7290e73857c6c3961b2cc183fad
Tomás Pietravallo <hello@tomaspietravallo.com>
2024-05-30 20:07:14 -0300

    colored graph: vpp vs frequency

--- main.py
+++ main.py
@@ -25,6 +25,22 @@ def escribir_archivo_csv(ruta_archivo, valores_x, valores_y):
         for i in range(len(valores_x)):
             escritor.writerow([valores_x[i], valores_y[i]])
 
+def hue_to_rgb(hue):
+    "Convierte un valor [0-1] a un color rgb"
+    hue = hue % 1.0
+    
+    h = hue * 6
+    x = 1 - abs(h % 2 - 1)
+    r, g, b = {
+        0: (1, x, 0),
+        1: (x, 1, 0),
+        2: (0, 1, x),
+        3: (0, x, 1),
+        4: (x, 0, 1),
+        5: (1, 0, x)
+    }[int(h) % 6]
+    
+    return (r, g, b)
 
 def generar_grafico(valores_x, valores_y, vpp, frecuencia, nombre_archivo):
     plt.figure(figsize=(10, 6))
@@ -87,7 +103,7 @@ Path('graficos').mkdir(exist_ok=True)
 # Leer todos los archivos CSV en la carpeta
 archivos_csv = sorted(ruta_carpeta.glob('*.CSV'), key=lambda x: os.path.basename(x))
 
-vpp_freq = []
+vpp_freq: list[float, float, list[int], list[int]] = []
 
 # Leer y almacenar los datos de waveform data y el periodo de muestreo de cada archivo CSV
 for idx, archivo in enumerate(archivos_csv):
@@ -98,7 +114,7 @@ for idx, archivo in enumerate(archivos_csv):
     escribir_archivo_csv(Path('datos') / f"{Path(archivo).stem}DatosOnda.csv", valores_x, valores_y)
 
     vpp, frecuencia = calcular_frecuencia(valores_x=valores_x, valores_y=valores_y, periodo_muestreo=periodo_muestreo)
-    vpp_freq.append([vpp, frecuencia])
+    vpp_freq.append([vpp, frecuencia, valores_x, valores_y])
 
     # Usar la función para generar gráficos
     generar_grafico(valores_x, valores_y, vpp, frecuencia, Path('graficos') / f"{Path(archivo).stem}DatosOnda.png")
@@ -108,11 +124,30 @@ for idx, archivo in enumerate(archivos_csv):
 
 # Genera un grafico de el VPP en funcion de la frecuencia
 plt.figure(figsize=(10, 6))
-plt.scatter(list(map(lambda x : x[1],vpp_freq)), list(map(lambda x : x[0],vpp_freq)))
-plt.title(f'Voltaje pico a pico en función de la frecuencia')
+plt.scatter(list(map(lambda x : x[1], vpp_freq)), list(map(lambda x : x[0], vpp_freq)))
+plt.title(f'Voltaje pico a pico en funcion de la Frecuencia')
 plt.xlabel('Frecuencia (kHz)')
-plt.ylabel('Vpp(V)')
+plt.ylabel('Voltaje pico a pico (V)')
 plt.grid(True)
 plt.ylim(0, 6)
 plt.savefig(Path('graficos') / f"Frecuencia_Vpp.png")
+plt.close()
+
+vpp_freq.sort(key=lambda x: x[0])
+plt.figure(figsize=(10,6))
+for id, vfxy in enumerate(vpp_freq):
+    if (
+        (id <= 15 and id % 5 == 0) or (id > 15 and id % 3 == 0)
+    ):
+        h = min(max(abs(vfxy[0] - 0.79) / (4.09 - 0.79), 0), 1) / 2.5
+        print(h, vfxy[1])
+        plt.scatter(vfxy[2], vfxy[3], color=hue_to_rgb(h))
+
+plt.title(f'Voltaje pico en funcion de la frecuencia')
+plt.figtext(x=0.5, y=0.01, ha="center", s="Frecuencias representadas por un grandiente linear de color. Rojo: 0.79kHz. Verde: 4.09kHz")
+plt.xlabel('Tiempo (\u00B5s)')
+plt.ylabel('Voltaje pico a pico (V)')
+plt.grid(True)
+plt.ylim(-4, 4)
+plt.savefig(Path('graficos') / f"DatosOnda.png")
 plt.close()
